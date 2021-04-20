cc001956a7fe586ecf0c04b9cb6d4e9b03c6e5b7
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-04-20 17:58:49 +0300

    Tested

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleUI
         {
             //Burada parantez içinde new InMemory'i artık EntityFramework kullandığımız için değiştirirek tüm teknolojiyi değiştirmiş olduk.
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetAllByCategoryId(2))
             {
                 Console.WriteLine(product.ProductName);
             }
