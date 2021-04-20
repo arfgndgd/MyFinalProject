973ed526f1e65eaddb1f08e25f1c7d84b2039f27
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-04-20 15:55:52 +0300

    Generic constraints added

---
+++ DataAccess/Abstract/ICustomerDal.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //Generic Repository Design Pattern
+    public interface ICustomerDal:IEntityRepository<Customer>
+    {
+    }
+}
--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -7,8 +8,19 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    //Generic Repository Design Pattern
-    public interface IEntityRepository<T>
+    /*
+     Generic Repository Design Pattern <T>
+     
+     
+     Generic Constraint: Diğer abstract classlarda generic olarak yazılabilecek alan içerisine referans tip dışında farklı bir veri tipi yazmayı engellemek için bunu yaparız. Yani yalnızca class değil referans tip yazabiliriz demek.
+    where T:class
+                                      ***DataAccess/Abstract classlar için 
+    class = referans tiptir.
+    IEntity =  IEntity olabilir veya IEntity implement eden bir nesne olabilir
+    new () = new()'lenebilir olmalı 
+     */
+
+    public interface IEntityRepository<T> where T:class,IEntity,new ()
     {
         //Tüm ilgili verilerin listesi (filtre yok ise tüm datayı getirir "filter = null")
         List<T> GetAll(Expression<Func<T,bool>>filter = null);
