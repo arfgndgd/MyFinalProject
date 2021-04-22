78bff649a9d95f3297cbc8e5fb10e541c157c04a
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-04-23 02:07:38 +0300

    Core Utilities classes added

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -20,6 +21,12 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            _productDal.Add(product);
+            return new Result(true, "Ekleme başarılı");
+        }
+
         public List<Product> GetAll()
         {
             //İş Kodları
@@ -29,6 +36,11 @@ namespace Business.Concrete
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryID == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p=>p.ProductID == productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
